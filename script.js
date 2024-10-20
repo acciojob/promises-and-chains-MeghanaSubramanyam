12175ee90ae9076bb953cf7cbd06f4eb0106a278
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-10-20 16:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click',function(event){
+	event.preventDefault();
+	let age=document.getElementById('age').value;
+	let name=document.getElementById('name').value;
+
+	new Promise((res,rej)=>{
+		setTimeout(()=>{
+			if(age>=18){
+				res(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				rej(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000)
+	})
+	.then(mes=>alert(mes));
+	.catch(mes=>alert(mes));
+})
